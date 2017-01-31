25cb323cb9efb0e0f28678ead1c767cbd8e93932
EetuDoesCoding <eetuelo@gmail.com>
2017-01-31 12:21:05 +0200

    päivitys 31-1 12:20

--- etusivu.php
+++ etusivu.php
@@ -7,12 +7,36 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation-icons/3.0/foundaiton-icons.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
-<h1>jaa</h1>
-<h2>jaa</h2>
-<h3>jaa</h3>
 
 <header>
+
+</header>
+
+<nav>
+<ul>
+<li><a href="">Etusivu</li>
+<li><a href="">Me</li>
+<li><a href="">Jäsenyys</li>
+<li><a href="">Aktiviteetit</li>
+<li><a href="">Matkailu</li>
+<li><a href="">Julkaisut</li>
+<li><a href="">FAQ</li>
+<li><a href="">Kumppanit</li>
+<li><a href="">Yhteystiedot</li>
+<li><a href="">Intranet</li>
+</ul>
+</nav>
+
+<article>
+
+</article>
+
+<footer>
+
+</footer>
+
 <script src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
 </body>
 </html>
