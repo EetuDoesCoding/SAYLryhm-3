6db8ea8006b8e5ce7d7b1f76985f6ecef4a33082
EetuDoesCoding <eetuelo@gmail.com>
2017-01-31 13:41:43 +0200

    EE viimeinen päivitys koulusta

--- pohja.php
+++ pohja.php
@@ -25,7 +25,7 @@
 <li><a href="">FAQ</a></li>
 <li><a href="">Kumppanit</a></li>
 <li><a href="">Yhteystiedot</a></li>
-<li><a href="">Intranet</a></li>
+<li><a href="http://sayl.fi/intranet/">Intranet</a></li>
 <li><a href=""><img src="usakuva.jpg" alt=""></a></li>
 </ul>
 </nav>
