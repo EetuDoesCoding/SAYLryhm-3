131a15b32b8b37c703e5b1feaa53a5ad249d137c
Eetu Eloranta <eetueloranta@space.space.kpedu.fi>
2017-01-31 10:45:46 +0200

    päivitys

---
+++ etusivu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php $_SERVER['PHP_SELF'] ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundaiton.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation-icons/3.0/foundaiton-icons.min.css">
+  <style>
+  <?php
+
+  ?>
+  </style>
+</head>
+<body>
+
+<script src="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.js"></script>
+</body>
+</html>
